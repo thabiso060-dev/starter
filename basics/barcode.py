be2cdc98c588c4506906dbb11a0d3d82d2498d1b
thabiso060-dev <jamakasjadu068@gmail.com>
2026-04-26 14:05:35 +0200

    Add files via upload

---
+++ basics/barcode.py
@@ -0,0 +1,14 @@
+import random
+
+a = random.randint(0,9)
+b = random.randint(0,9)
+c = random.randint(0,9)
+d = random.randint(0,9)
+e = random.randint(0,9)
+f = random.randint(0,9)
+g = random.randint(0,9)
+h = random.randint(0,9)
+i = random.randint(0,9)
+j = random.randint(0,9)
+
+print(a,b,c,d,e,f,g,h,i,j, sep=" ")
---
+++ basics/casino 1.py
@@ -0,0 +1,9 @@
+for i in range(3):
+   import random
+   r = random.randint(1,50)
+   t = random.randint(1,50)
+   n = random.randint(1,50)
+   g = random.randint(1,50)
+   p = random.randint(1,50)
+   y = random.randint(1,50)
+   print(r,t,n,g,p," ",y)
